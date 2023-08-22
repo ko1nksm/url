b1d3c53c9237469f5e426115af20919af4b644ea
Koichi Nakashima <koichi@nksm.name>
2023-08-22 21:21:39 +0900

    Add -s, -n, -f options for urlprintf

--- urllib.sh
+++ urllib.sh
@@ -1,28 +1,19 @@
 # shellcheck shell=sh
 
 ##########################################################################
-# urlprintf is released under the BSD Zero Clause License
+# sh-urllib is released under the BSD Zero Clause License
 # https://github.com/ko1nksm/sh-urllib
 ##########################################################################
 urlprintf() {
   [ $# -eq 0 ] && echo "urlprintf: not enough arguments" >&2 && return 1
-  {
-    shift
-    LC_ALL=C awk '
-      function encode(map, str,   i, len, ret) {
-        len = length(str); ret = ""
-        for (i = 1; i <= len; i++) ret = ret map[substr(str, i, 1)]
-        return ret
-      }
-      BEGIN {
-        for(i = 0; i < 256; i++) {
-          k = sprintf("%c", i); v = sprintf("%%%02X", i)
-          url[k] = (k ~ /[A-Za-z0-9_.~-]/) ? k : v
-        }
-        for (i = 1; i < ARGC; i++) print encode(url, ARGV[i])
-      }
-    ' "$@"
-  } | (
+  _urllib_urlencode printf "$@" | (
+    while [ $# -gt 0 ]; do
+      case $1 in
+        --) shift && break ;;
+        -*) shift ;;
+        *) break ;;
+      esac
+    done
     set -- "$1"
     while IFS= read -r line; do
       set -- "$@" "$line"
@@ -32,19 +23,20 @@ urlprintf() {
   )
 }
 
-##########################################################################
-# urlbuild is released under the BSD Zero Clause License
-# https://github.com/ko1nksm/sh-urllib
-##########################################################################
 urlbuild() {
+  _urllib_urlencode build "$@"
+}
+
+_urllib_urlencode() {
   LC_ALL=C awk '
     function encode(map, str,   i, len, ret) {
       len = length(str); ret = ""
       for (i = 1; i <= len; i++) ret = ret map[substr(str, i, 1)]
       return ret
     }
+
     function parse_options(i) {
-      i = 1; space = 0; newline = 0
+      space = 0; newline = 0
       while (ARGV[i] ~ /^-/ && i < ARGC) {
         if (ARGV[i] ~ /s/) space = 1
         if (ARGV[i] ~ /n/) newline = 1
@@ -53,22 +45,19 @@ urlbuild() {
       }
       return i
     }
+
     function tocrlf(i) {
       for (i = 1; i < ARGC; i++) {
         gsub(/\r\n/, "\n", ARGV[i])
         gsub(/\n/, "\r\n", ARGV[i])
       }
     }
-    BEGIN {
-      for(i = 0; i < 256; i++) {
-        k = sprintf("%c", i); v = sprintf("%%%02X", i)
-        uri[k] = (k ~ /[A-Za-z0-9_.!~*\47();\/?:@&=+$,#-]/) ? k : v
-        url[k] = (k ~ /[A-Za-z0-9_.~-]/) ? k : v
-      }
 
-      i = parse_options()
-      if (space) uri[" "] = url[" "] = "+"
-      if (newline) tocrlf()
+    function urlprintf() {
+      for (i++; i < ARGC; i++) print encode(url, ARGV[i])
+    }
+
+    function urlbuild() {
       path = encode(uri, ARGV[i++]); params = fragment = ""
       for (; i < ARGC; i++) {
         if (sub(/^-/, "", ARGV[i])) {
@@ -81,6 +70,22 @@ urlbuild() {
         }
       }
       printf "%s%s%s\n", path, (params ? "?" : "") params, fragment
+    }
+
+    BEGIN {
+      for(i = 0; i < 256; i++) {
+        k = sprintf("%c", i); v = sprintf("%%%02X", i)
+        uri[k] = (k ~ /[A-Za-z0-9_.!~*\47();\/?:@&=+$,#-]/) ? k : v
+        url[k] = (k ~ /[A-Za-z0-9_.~-]/) ? k : v
+      }
+
+      mode = ARGV[1]
+      i = parse_options(2)
+      if (space) uri[" "] = url[" "] = "+"
+      if (newline) tocrlf()
+
+      if (mode == "printf") urlprintf()
+      if (mode == "build") urlbuild()
     }
   ' "$@"
 }
