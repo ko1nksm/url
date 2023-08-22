8ea2bda773d1746847a3e35cbb6e5bf6abde34a8
Koichi Nakashima <koichi@nksm.name>
2023-08-22 20:14:48 +0900

    Fix test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -eu
 
 . ./urllib.sh
 
-urlprintf "http://example.com?param1=%s&param2=%s\n" あ ａ ア Ａ  
-urlbuild "http://example.com" -param1 あ -param2 "か" "#さ"
-urlbuild "http://example.com" =あいうえお
+urlprintf "http://example.com/?param1=%s&param2=%s\n" あ ａ ア Ａ
+urlbuild "http://example.com/" -param1 あ -param2 "か" "#さ"
+urlbuild "http://example.com/" =あいうえお
 
