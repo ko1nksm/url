be6b225c14e6ecaa2607b253aff46d1a6cc0cf4d
Koichi Nakashima <koichi@nksm.name>
2023-08-22 20:51:43 +0900

    Add -s, -n, -f options for urlbuild

--- urllib.sh
+++ urllib.sh
@@ -43,14 +43,34 @@ urlbuild() {
       for (i = 1; i <= len; i++) ret = ret map[substr(str, i, 1)]
       return ret
     }
+    function parse_options(i) {
+      i = 1; space = 0; newline = 0
+      while (ARGV[i] ~ /^-/ && i < ARGC) {
+        if (ARGV[i] ~ /s/) space = 1
+        if (ARGV[i] ~ /n/) newline = 1
+        if (ARGV[i] ~ /f/) space = newline = 1
+        if (ARGV[i++] == "--") break
+      }
+      return i
+    }
+    function tocrlf(i) {
+      for (i = 1; i < ARGC; i++) {
+        gsub(/\r\n/, "\n", ARGV[i])
+        gsub(/\n/, "\r\n", ARGV[i])
+      }
+    }
     BEGIN {
       for(i = 0; i < 256; i++) {
         k = sprintf("%c", i); v = sprintf("%%%02X", i)
         uri[k] = (k ~ /[A-Za-z0-9_.!~*\47();\/?:@&=+$,#-]/) ? k : v
         url[k] = (k ~ /[A-Za-z0-9_.~-]/) ? k : v
       }
-      path = encode(uri, ARGV[1]); params = fragment = ""
-      for (i = 2; i < ARGC; i++) {
+
+      i = parse_options()
+      if (space) uri[" "] = url[" "] = "+"
+      if (newline) tocrlf()
+      path = encode(uri, ARGV[i++]); params = fragment = ""
+      for (; i < ARGC; i++) {
         if (sub(/^-/, "", ARGV[i])) {
           if (params) params = params "&"
           params = params encode(url, ARGV[i]) "=" encode(url, ARGV[++i])
